a063865b428e2aef10a6e12590df62a7e22ae82f
Tal Hayut <talhayut.dev@gmail.com>
2020-02-25 11:55:37 +0200

    feat: adding colors

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ class Chartscii {
         this.createGraphAxis();
         this.colors = {
             green: '\x1b[32;1m',
+            red: '\x1b[31;1m',
+            cyan: '\x1b[96;1m',
+            pink: '\x1b[95;1m',
+            blue: '\x1b[34;1m',
+            yellow: '\x1b[33;1m',
             reset: '\x1b[0m'
         }
     }
@@ -64,7 +69,9 @@ class Chartscii {
         if (this.maxSpace % 2 === 0) {
             return ' '.repeat(this.maxSpace)
         } else if (this.maxSpace % 3 === 0) {
-            return this.maxSpace === 3 ? ' ' : ' '.repeat(this.maxSpace - 7)
+            return this.maxSpace === 3
+                ? ' '
+                : ' '.repeat(this.maxSpace - 7)
         } else if (this.maxSpace < 2) {
             return ' '.repeat(this.maxSpace + 1);
         } else {
--- test.js
+++ test.js
@@ -5,9 +5,9 @@ let count = 0;
 const data = [];
 
 for (let i = 1; i <= 10; i++) {
-    data.push({ value: Math.floor(Math.random() * 6) + 1, label: `item ${count++}` });
+    data.push({ value: Math.floor(Math.random() * 6) + 1, label:`label_${count++}` });
 }
 
 // create chart
-const chart = new Chartscii(data, { label: 'CPU', width: 300, sort: false, reverse: false, color: 'green' });
+const chart = new Chartscii(data, { label: 'Jordan expenses', width: 300, sort: false, reverse: false, color: 'cyan' });
 console.log(chart.create());
