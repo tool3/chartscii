9fbc03e83a8c7c74d259869ad519ace77f1acade
Tal Hayut <talhayut.dev@gmail.com>
2020-03-01 21:05:33 +0200

    chore: test fix

--- index.test.js
+++ index.test.js
@@ -11,19 +11,19 @@ describe('chartscii tests', () => {
     });
 
     it('should initialize graph array with all values', () => {
-        const result = chart.toString();
+        const result = chart.get();
         expect(result.length).to.equal(10);
     });
 
     it('should support 0 values', () => {
-        const result = chart.toString();
+        const result = chart.get();
         expect(result[0]).to.include({ value: "0" });
     });
 
     it('should support { value }', () => {
         const data = [...Array(10).keys()].map(key => { return { value: key } });
         chart = new Chartscii(data);
-        const result = chart.toString()[0];
+        const result = chart.get()[0];
         expect(result.value).to.equal('0');
     });
 
@@ -31,7 +31,7 @@ describe('chartscii tests', () => {
         let counter = 0;
         const data = [...Array(10).keys()].map(key => { return { value: key, label: `label ${counter++}` } });
         chart = new Chartscii(data);
-        const result = chart.toString()[0];
+        const result = chart.get()[0];
         expect(result.label).to.equal('label 0');
     });
 
@@ -39,7 +39,7 @@ describe('chartscii tests', () => {
         let counter = 0;
         const data = [...Array(10).keys()].map(key => { return { value: key, label: `label ${counter++}` } });
         chart = new Chartscii(data, { percentage: true });
-        const result = chart.toString()[0];
+        const result = chart.get()[0];
         expect(result.label).to.include('%');
     });
 });
