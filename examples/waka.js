6963a0c8836a0ddc5add6ffc59f308812d851432
Tal Hayut <talhayut.dev@gmail.com>
2020-03-02 11:19:14 +0200

    docs: adding wakatime text output

--- examples/waka.js
+++ examples/waka.js
@@ -1,6 +1,6 @@
 const Chartscii = require('../index');
 
-const waka = 'your api call to get last 7 days waka stats: https://wakatime.com/developers/#stats'
+const waka = require('../../waka.mock')
 const { languages }  = waka.data;
 
 const data = languages.map(lang => {
