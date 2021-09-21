10b5cf3bcf1fe6d7aee88c940340fb299816e54f
Tal Hayut <talha@accessibe.com>
2021-09-21 12:42:36 +0300

    fix: new logo

--- test.js
+++ test.js
@@ -1,40 +1,19 @@
-// const style = require('styl3')({theme: 'pastel'});
-// // const gradient = require('gradient-string');
 const shellfie = require('shellfie');
-// const logo = [
-// `║ ${style.pink`████ █  █ ████ ████ █████ ████ ████ █ █`}`,
-// `║ ${style.pink`█    █  █ █  █ █  █   █   █    █    █ █`}`,
-// `║ ${style.pink`█    ████ ████ ███    █   ████ █    █ █`}`,
-// `║ ${style.pink`█    █  █ █  █ █  █   █      █ █    █ █`}`,
-// `║ ${style.pink`████ █  █ █  █ █  █   █   ████ ████ █ █`}`,
-// '║',
-// `║ ${style.purple`████████████`}`,
-// `║ ${style.pink`███████████████████████████████████████`}`,
-// `║ ${style.green`████████████████████████████`}`,
-// `║ ${style.blue`████████████████████████████████████`}`,
-// '╚════════════════════════════════════════',
-// '       create beautiful ascii charts'
-// ]
+const gradient = require('gradient-string');
 
+const banner = `
+ ║╔═╗ ╦ ╦ ╔═╗ ╦═╗ ╔╦╗ ╔═╗ ╔═╗ ╦ ╦
+ ║║   ╠═╣ ╠═╣ ╠╦╝  ║  ╚═╗ ║   ║ ║ 
+ ║╚═╝ ╩ ╩ ╩ ╩ ╩╚═  ╩  ╚═╝ ╚═╝ ╩ ╩ 
+ ║███████████████████████████████
+ ║██████████████████████
+ ║████████████
+ ╚════════════════════════════════
+       awesome ascii charts
+`
+
+const formatted = gradient('#ffff', '#e684ae')(banner);
 async function run (logo, name) {
-    await shellfie(logo, {viewport: {height: 500, width: 520}, name});
+    await shellfie(logo, {viewport: {height: 250, width: 350}, name});
 }
-const data = [];
-
-for (let i = 1; i <= 20; i++) {
-    data.push(Math.floor(Math.random() * 100) + 1);
-}
-// run()
-const Chartscii = require('./')
-const chart = new Chartscii(data, {
-    label: 'Example Chart',
-    theme: 'pastel',
-    width: 50,
-    char: '■',
-    sort: true,
-    reverse: true,
-    color: 'green'
-});
-
-// console.log(chart.create());
-run(chart.create(), 'char')
+run(formatted, 'logo');
