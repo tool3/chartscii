d5234c8c06aa8b3cd483319e1da8ec67ea5078c4
Tal Hayut <talha@accessibe.com>
2021-10-01 20:51:31 +0300

    chore: welcome image

--- test.js
+++ test.js
@@ -2,18 +2,25 @@ const shellfie = require('shellfie');
 const gradient = require('gradient-string');
 
 const banner = `
- ║╔═╗ ╦ ╦ ╔═╗ ╦═╗ ╔╦╗ ╔═╗ ╔═╗ ╦ ╦
- ║║   ╠═╣ ╠═╣ ╠╦╝  ║  ╚═╗ ║   ║ ║ 
- ║╚═╝ ╩ ╩ ╩ ╩ ╩╚═  ╩  ╚═╝ ╚═╝ ╩ ╩ 
- ║███████████████████████████████
- ║██████████████████████
- ║████████████
- ╚════════════════════════════════
-       awesome ascii charts
+║╔═╗ ╦ ╦ ╔═╗ ╦═╗ ╔╦╗ ╔═╗ ╔═╗ ╦ ╦
+║║   ╠═╣ ╠═╣ ╠╦╝  ║  ╚═╗ ║   ║ ║ 
+║╚═╝ ╩ ╩ ╩ ╩ ╩╚═  ╩  ╚═╝ ╚═╝ ╩ ╩ 
+║███████████████████████████████
+║██████████████████████
+║████████████
+╚═══════════════════════════════
+    awesome ascii charts
 `
 
 const formatted = gradient('#ffff', '#e684ae')(banner);
 async function run (logo, name) {
-    await shellfie(logo, {viewport: {height: 250, width: 350}, name});
+    await shellfie(logo, {viewport: {height: 250, width: 370}, name, rendererType: 'dom',
+    style: {
+        'text-align': 'center',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 }
 run(formatted, 'logo');
